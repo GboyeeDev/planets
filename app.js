ef31ae1960bf2d01afa732c8732ca85dec0bc63c
Olugboyega <olugboyega.ojelabi@gmail.com>
2023-03-03 22:52:34 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const { parse } = require('csv-parse');
+const fs = require('fs')
+
+// this array will receive our data.
+const habitablePlanets = [];
+
+function isHabitablePlanets(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+      && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+      && planet['koi_prad'] < 1.6;
+}
+
+
+// rading kepler_data as a stream
+fs.createReadStream('kepler_data.csv')
+    // pipe is the destination of my kepler_data
+    .pipe(parse({
+        comment: '#',
+        column: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanets(data)) {
+            habitablePlanets.push(data);
+        } 
+    })
+    .on('error', (err) => {
+        console.log(err)
+    })
+    .on('end', () => {
+        // used map method to get the planets name from the data we got earlier
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }))
+        console.log(`${habitablePlanets.length} habitable planets found! `)
+    });
+
+
+
+//parse();
+
