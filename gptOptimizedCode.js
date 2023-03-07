ec9cfd3ed913335ba96b95af3a7c18aa2d661a22
Olugboyega <114001863+GboyeeDev@users.noreply.github.com>
2023-03-07 10:42:30 +0100

    Update gptOptimizedCode.js

--- gptOptimizedCode.js
+++ gptOptimizedCode.js
@@ -6,7 +6,10 @@ const habitablePlanets = [];
 function isHabitablePlanets(planet) {
   return (
     // from keplers_data
-    
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
   );
 }
 
